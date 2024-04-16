c0fa88ccd8b5f6cd980763401b9fe80619c84c0c
yassir5236 <yassiraitlahmadi@gmail.com>
2024-04-16 19:21:45 +0100

    adding panier fucntion complet need  juste devis

--- gym2/resources/views/clients/products/detail.blade.php
+++ gym2/resources/views/clients/products/detail.blade.php
@@ -1,32 +1,23 @@
 @extends('layout')
 
 @section('content')
-    <div class="container mx-auto py-8">
-        <div class="bg-white shadow-lg overflow-hidden">
-            <img src="{{ asset('storage/' . $product->image_path) }}" alt="{{ $product->name }}" class="w-full h-64 object-cover">
+    <div class="container mx-auto px-4 py-8 max-w-xl">
+        <div class="bg-white shadow-lg overflow-hidden rounded-lg">
+            <img src="{{ asset('storage/' . $product->image_path) }}" alt="{{ $product->name }}" class="w-full h-64 object-cover object-center">
             <div class="p-6">
-                <h3 class="text-xl font-semibold text-gray-900 mb-2">{{ $product->name }}</h3>
+                <h3 class="text-2xl font-semibold text-gray-900 mb-2">{{ $product->name }}</h3>
                 <p class="text-gray-700 mb-4">{{ $product->description }}</p>
                 <p class="text-gray-800 font-semibold">Price: ${{ number_format($product->price, 2) }}</p>
                 <p class="text-gray-800">Quantity Available: {{ $product->quantity }}</p>
-            
 
-                {{-- <form action="{{ route('cart.add', $product->id) }}" method="POST">
+                <form action="{{ route('cart.add', $product->id) }}" method="POST" id="add-to-cart-form" class="flex items-center mt-4">
                     @csrf
-                    <label for="quantity">Quantity:</label>
-                    <input type="number" name="quantity" id="quantity" value="1" min="1">
-                    <button type="submit">Add to Cart</button>
-                </form> --}}
-
-                <form action="{{ route('cart.add', $product->id) }}" method="POST" id="add-to-cart-form">
-                    @csrf
-                    <label for="quantity">Quantity:</label>
-                    <input type="number" name="quantity" id="quantity" value="1" min="1">
-                    <button type="submit" id="add-to-cart-btn">Add to Cart</button>
+                    <label for="quantity" class="mr-2">Quantity:</label>
+                    <input type="number" name="quantity" id="quantity" value="1" min="1" class="w-16 px-2 py-1 border border-gray-300 rounded">
+                    <button type="submit" id="add-to-cart-btn" class="ml-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Add to Cart</button>
                 </form>
             </div>
         </div>
     </div>
-
-
 @endsection
+
--- gym2/resources/views/clients/products/panier.blade.php
+++ gym2/resources/views/clients/products/panier.blade.php
@@ -77,56 +77,64 @@
     <div class="container mx-auto py-6">
         <h1 class="text-3xl font-bold mb-4">Shopping Cart</h1>
 
-        <div class="flex flex-col md:flex-row md:space-x-6">
-            <div class="w-full md:w-3/4">
+        <div class="flex justify-between md:space-x-6">
+            <div class=" flex   w-full md:w-3/4">
                 @if (session('cart'))
                     @php
                         $totalPrice = 0;
                     @endphp
                     @foreach (session('cart') as $id => $item)
-                        <div class="bg-white rounded shadow-md mb-4">
-                            <div class="p-4">
+                        <div class=" mb-10 max-w-2xl mx-auto">
+                            <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+
+                                <img src="{{ asset('storage/' . $item['image_path']) }}" alt="{{ $item['name'] }}"
+                                    class="my-2">
+                                <div class="px-5 mb-10 pb-5">
+                                    <h5 class="text-lg font-semibold">{{ $item['name'] }}</h5>
+
+                                    <p class="text-gray-600">Price: ${{ $item['price'] }}</p>
 
 
-
-
-                                <img src="{{asset('storage/'. $item['image_path']) }}" alt="{{ $item['name'] }}" class="my-2">
-
-                                <h5 class="text-lg font-semibold">{{ $item['name'] }}</h5>
-
-                                <p class="text-gray-600">Price: ${{ $item['price'] }}</p>
-                                <div class="flex items-center">
-                                    <button class="btn-decrease" data-id="{{ $id }}">-</button>
-                                    <p class="text-gray-600 quantity">{{ $item['quantity'] }}</p>
-                                    <button class="btn-increase" data-id="{{ $id }}">+</button>
+                                    <div class="flex items-center">
+                                        <button
+                                            class="px-4 py-2 bg-gray-300 text-white rounded-l-md focus:outline-none focus:ring focus:ring-blue-400 btn-decrease"
+                                            data-id="{{ $id }}">-</button>
+                                        <p class="text-gray-600 px-4">{{ $item['quantity'] }}</p>
+                                        <button
+                                            class="px-4 py-2 bg-gray-300 text-white rounded-r-md focus:outline-none focus:ring focus:ring-blue-400 btn-increase"
+                                            data-id="{{ $id }}">+</button>
+                                    </div>
+                                    <p class="text-gray-600 totalprice text-3xl font-bold text-gray-900 dark:text-white">
+                                        ${{ $item['price'] * $item['quantity'] }}</p>
+                                    <form action="{{ route('cart.remove', $id) }}" method="POST">
+                                        @csrf
+                                        <button type="submit" style="background-color: coral"
+                                            class=" text-white font-semibold px-4 py-2 mt-2 rounded">Remove</button>
+                                    </form>
                                 </div>
-                                <p class="text-gray-600 totalprice">${{ $item['price'] * $item['quantity'] }}</p>
-                                <form action="{{ route('cart.remove', $id) }}" method="POST">
-                                    @csrf
-                                    <button type="submit"
-                                        class="bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 mt-2 rounded">Remove</button>
-                                </form>
                             </div>
-                        </div>
                     @endforeach
                 @else
                     <p class="text-gray-600">Your cart is empty</p>
                 @endif
             </div>
-
-            <div class="w-full md:w-1/4">
-                @if (session('cart'))
-                    <div class="bg-white rounded shadow-md p-4 ">
-                        <h5 class="text-lg font-semibold mb-4">Total</h5>
-                        <p class="text-gray-600" id="finalPrice">Final Price: ${{ $totalPrice }}</p>
-                        <a href="{{ route('cart.checkout') }}"
-                            class="block bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 mt-4 rounded">Proceed
-                            to Checkout</a>
-                    </div>
-                @endif
-            </div>
+        </div>
+        <div class="w-full md:w-1/4 ">
+            @if (session('cart'))
+                <div class="bg-white rounded shadow-md mt-2 p-4 ">
+                    <h5 class="text-lg  font-semibold mb-4">Total</h5>
+                    <p class="text-gray-600  text-2xl" id="finalPrice">Final Price: ${{ $totalPrice }}</p>
+                    <a href="{{ route('cart.checkout') }}"
+                        class="block bg-red-700 hover:bg-red-600 text-white font-semibold px-4 py-2 mt-4 rounded">Proceed
+                        to Checkout</a>
+                </div>
+            @endif
         </div>
     </div>
+
+
+
+
 
     {{-- <script>
         
@@ -301,79 +309,78 @@
 
     <script>
         document.addEventListener('DOMContentLoaded', function() {
-    const increaseButtons = document.querySelectorAll('.btn-increase');
-    const decreaseButtons = document.querySelectorAll('.btn-decrease');
+            const increaseButtons = document.querySelectorAll('.btn-increase');
+            const decreaseButtons = document.querySelectorAll('.btn-decrease');
 
-    increaseButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const id = this.getAttribute('data-id');
-            updateQuantity(id, 'increase');
-        });
-    });
+            increaseButtons.forEach(button => {
+                button.addEventListener('click', function() {
+                    const id = this.getAttribute('data-id');
+                    updateQuantity(id, 'increase');
+                });
+            });
 
-    decreaseButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const id = this.getAttribute('data-id');
-            updateQuantity(id, 'decrease');
-        });
-    });
+            decreaseButtons.forEach(button => {
+                button.addEventListener('click', function() {
+                    const id = this.getAttribute('data-id');
+                    updateQuantity(id, 'decrease');
+                });
+            });
 
-    // Fonction pour calculer et afficher le prix total
-    function updateTotalPrice() {
-        const totalPrices = document.querySelectorAll('.totalprice');
-        let totalPriceSum = 0;
-        totalPrices.forEach(totalPrice => {
-            totalPriceSum += parseFloat(totalPrice.textContent.replace('$', ''));
-        });
+            // Fonction pour calculer et afficher le prix total
+            function updateTotalPrice() {
+                const totalPrices = document.querySelectorAll('.totalprice');
+                let totalPriceSum = 0;
+                totalPrices.forEach(totalPrice => {
+                    totalPriceSum += parseFloat(totalPrice.textContent.replace('$', ''));
+                });
 
-        const finalPriceElement = document.querySelector('#finalPrice');
-        finalPriceElement.textContent = '$' + totalPriceSum.toFixed(2);
-    }
-
-    // Appeler la fonction pour calculer et afficher le prix total une fois la page chargée
-    updateTotalPrice();
-
-    function updateQuantity(id, action) {
-        const quantityElement = document.querySelector(`[data-id="${id}"]`).nextElementSibling;
-        let newQuantity = parseInt(quantityElement.textContent);
-        if (action === 'increase') {
-            newQuantity += 1;
-        } else if (action === 'decrease' && newQuantity > 1) {
-            newQuantity -= 1;
-        } else {
-            return;
-        }
-
-        fetch(`/cart/update/${id}`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRF-TOKEN': '{{ csrf_token() }}'
-            },
-            body: JSON.stringify({
-                quantity: newQuantity
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                quantityElement.textContent = newQuantity;
-
-                const totalPriceElement = quantityElement.parentElement.nextElementSibling;
-                const priceElement = quantityElement.parentElement.previousElementSibling;
-                const price = parseFloat(priceElement.textContent.trim().replace('Price: $', ''));
-                const totalPrice = parseFloat(price * newQuantity);
-                totalPriceElement.textContent = '$' + totalPrice.toFixed(2);
-
-                // Mettre à jour le prix total
-                updateTotalPrice();
-            } else {
-                console.error(data.message);
+                const finalPriceElement = document.querySelector('#finalPrice');
+                finalPriceElement.textContent = '$' + totalPriceSum.toFixed(2);
             }
-        })
-        .catch(error => console.error(error));
-    }
-});
 
+            // Appeler la fonction pour calculer et afficher le prix total une fois la page chargée
+            updateTotalPrice();
+
+            function updateQuantity(id, action) {
+                const quantityElement = document.querySelector(`[data-id="${id}"]`).nextElementSibling;
+                let newQuantity = parseInt(quantityElement.textContent);
+                if (action === 'increase') {
+                    newQuantity += 1;
+                } else if (action === 'decrease' && newQuantity > 1) {
+                    newQuantity -= 1;
+                } else {
+                    return;
+                }
+
+                fetch(`/cart/update/${id}`, {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                        },
+                        body: JSON.stringify({
+                            quantity: newQuantity
+                        })
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.success) {
+                            quantityElement.textContent = newQuantity;
+
+                            const totalPriceElement = quantityElement.parentElement.nextElementSibling;
+                            const priceElement = quantityElement.parentElement.previousElementSibling;
+                            const price = parseFloat(priceElement.textContent.trim().replace('Price: $', ''));
+                            const totalPrice = parseFloat(price * newQuantity);
+                            totalPriceElement.textContent = '$' + totalPrice.toFixed(2);
+
+                            // Mettre à jour le prix total
+                            updateTotalPrice();
+                        } else {
+                            console.error(data.message);
+                        }
+                    })
+                    .catch(error => console.error(error));
+            }
+        });
     </script>
 @endsection
